c62aa01d1528ec827db766c751bca43f6302815f
Raphael Cavalcanti <rapscavs@gmail.com>
2022-07-20 15:33:09 -0300

    Testing middleware cookies

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,29 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth({
   callbacks: {
     authorized: ({ req, token }) => {
-      console.log('req', req);
-      console.log('token', token);
+      console.log('token!', token);
+      console.log('token?.userRole!', token?.userRole);
+  
+      const secret = process.env.NEXTAUTH_SECRET;
+      console.log('secret!', secret);
+
+      const { cookies } = req;
+      console.log('cookies!', cookies);
+      console.log('cookies instanceof Map!', cookies instanceof Map);
+      console.log('cookies.keys()!', cookies.keys());
+
+      const cookieSessionToken = cookies.get('__Secure-next-auth.session-token');
+      console.log('cookie "__Secure-next-auth.session-token"!', cookieSessionToken);
+
+      const cookieCallbackUrl = cookies.get('__Secure-next-auth.callback-url');
+      console.log('cookie "__Secure-next-auth.callback-url"!', cookieCallbackUrl);
+
+      const cookieCsrfToken = cookies.get('__Host-next-auth.csrf-token');
+      console.log('cookie "__Host-next-auth.csrf-token"!', cookieCsrfToken);
+      
+      const allCookies = cookies.entries();
+      console.log('allCookies!', allCookies);
+
       // /admin requires admin role, but /me only requires the user to be logged in.
       return req.nextUrl.pathname !== "/admin" || token?.userRole === "admin";
     },
